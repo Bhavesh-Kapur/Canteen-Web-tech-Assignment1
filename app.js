71ba9c1508754d2a0ce461b1ebd7be037f4246a7
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2024-04-21 19:48:14 +0530

    vercel update3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require('ejs')
 const path = require('path');
 const port = 3000
 const app = express();
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
